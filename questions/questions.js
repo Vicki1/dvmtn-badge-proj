305815fdafe1887a31946ed209099644715c247f
Victoria Yorgesen <victoria.yorgesen1@gmail.com>
2017-10-30 09:46:47 -0600

    in progress

---
+++ questions/questions.js
@@ -0,0 +1,37 @@
+/*
+********************
+
+
+
+Student can explain what web semantics are and can apply best practices in their HTML files.
+Web semantics are the tags that we use  to structure HTML on a webpage. They are 
+some of the most basic building pieces of a webpage, and give web developers a 
+common base to start off of when building websites. Tags that are outside the body
+ are often expected to be laid out with a specific relation to each other. For example, 
+ title and metadata  tags will go within the head of the page, which will be within the 
+ html tags, but before the body tags. Tags that go within the body, such as header, div,
+  paragraph, and span tags can be laid out in almost any number of ways, as long as they 
+  are inside the body. It is best practice to lay out the web semantic tags in a way 
+  which will make it easy to make use of the box model when it comes to styling.
+  
+ *******************
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/ 
